21d1dfaa025f495bfbed0236e4859c4a4f17682d
Kevin <hizkiakevin8@gmail.com>
2024-10-14 23:20:10 +0700

    add guided - soal_latihan1.go

--- praktikum_3/guided/soal_1/soal_latihan1.go
+++ praktikum_3/guided/soal_1/soal_latihan1.go
@@ -2,28 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
-
-	urutanBenar := []string{"merah", "kuning", "hijau", "ungu"}
+func main (){ 
+	urutanBenar := []string{"merah","kuning","hijau","ungu"}
 	hasil := true
 
 	for i := 1; i <= 5; i++ {
 		var warna1, warna2, warna3, warna4 string
-		fmt.Printf("percobaan %d\n ", i)
-		fmt.Print("masukkan warna pertama: ")
-		fmt.Scan(&warna1)
-		fmt.Print("masukkan warna kedua: ")
-		fmt.Scan(&warna2)
-		fmt.Print("masukkan warna ketiga: ")
-		fmt.Scan(&warna3)
-		fmt.Print("masukkan warna keempat: ")
-		fmt.Scan(&warna4)
+		fmt.Printf("Percobaan %d\n", i)
+		fmt.Print("Masukan warna pertama : ")
+		fmt.Scanln(&warna1)
+		fmt.Print("Masukan warna kedua : ")
+		fmt.Scanln(&warna2)
+		fmt.Print("Masukan warna ketiga : ")
+		fmt.Scanln(&warna3)
+		fmt.Print("Masukan warna keempat : ")
+		fmt.Scanln(&warna4)
 
-		if warna1 != urutanBenar[0] || warna2 != urutanBenar[1] || warna3 != urutanBenar[2] || warna4 != urutanBenar[3] {
+		if warna1 != urutanBenar[0] || warna2 != urutanBenar[1] || warna3 != urutanBenar[2] || warna4 != urutanBenar[3]{
 			hasil = false
-			
 		}
 	}
-	println("BERHASIL", hasil)
-
+	fmt.Println("Berhasil : ", hasil)
 }
