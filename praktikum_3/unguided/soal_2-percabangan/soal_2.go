69f020e551486695e5105d94207925e842f2d12d
Kevin <hizkiakevin8@gmail.com>
2024-10-14 23:42:52 +0700

    add unguided | percabangan - soal_2.go

--- praktikum_3/unguided/soal_2-percabangan/soal_2.go
+++ praktikum_3/unguided/soal_2-percabangan/soal_2.go
@@ -10,17 +10,22 @@ func main() {
 
 	if nam > 80 {
 		nmk = "A"
-	} else if nam > 72.5 {
+	}
+	if nam > 72.5 {
 		nmk = "AB"
-	} else if nam > 65 {
+	}
+	if nam > 65 {
 		nmk = "B"
-	} else if nam > 57.5 {
+	}
+	if nam > 57.5 {
 		nmk = "BC"
-	} else if nam > 50 {
+	}
+	if nam > 50 {
 		nmk = "C"
-	} else if nam > 40 {
+	}
+	if nam > 40 {
 		nmk = "D"
-	} else {
+	} else if nam <= 40 {
 		nmk = "E"
 	}
 	fmt.Println("Nilai mata kuliah: ", nmk)
