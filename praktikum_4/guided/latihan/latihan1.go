7bd16c71ad94065d5b6c2f08442f0dff520f6103
Kevin <hizkiakevin8@gmail.com>
2024-10-22 13:52:28 +0700

    add guided program latihan

--- praktikum_4/guided/latihan/latihan1.go
+++ praktikum_4/guided/latihan/latihan1.go
@@ -2,24 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
-	var a,b int
-    fmt.Scan(&a, &b)
-    if a >= b {
-        fmt.Println(permutasi(a,b))
-    }else{
-        fmt.Println(permutasi(b,a))
-    }
+func faktorial(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	result := 1
+	for i := 2; i <= n; i++ {
+		result *= i
+	}
+	return result
 }
 
-func faktorial(n int) int{
-    var hasil int = 1
-    var i int
-    for i = 1; i <= n; i++ {
-            hasil = hasil * i
-    }
-    return hasil
+func permutasi(n int, r int) int {
+	return faktorial(n) / faktorial(n-r)
 }
-func permutasi(n,r int) int {
-    return faktorial(n) / faktorial(n-r)
-   }
+
+func kombinasi(n int, r int) int {
+	return faktorial(n) / (faktorial(r) * faktorial(n-r))
+}
+
+func main() {
+	var a, b, c, d int
+	
+	fmt.Println("Masukkan nilai a, b, c, d: ")
+	fmt.Scan(&a, &b, &c, &d)
+
+	p1 := permutasi(a, c)
+	c1 := kombinasi(a, c)
+	
+	p2 := permutasi(b, d)
+	c2 := kombinasi(b, d)
+
+	fmt.Printf("P(%d, %d) = %d\n", a, c, p1)
+	fmt.Printf("C(%d, %d) = %d\n", a, c, c1)
+	fmt.Printf("P(%d, %d) = %d\n", b, d, p2)
+	fmt.Printf("C(%d, %d) = %d\n", b, d, c2)
+}
