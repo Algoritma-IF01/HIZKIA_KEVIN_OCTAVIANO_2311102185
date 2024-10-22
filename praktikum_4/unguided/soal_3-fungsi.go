2fecd832e50140bbec2152a10c0f600ce074d49d
Kevin <hizkiakevin8@gmail.com>
2024-10-22 13:42:15 +0700

    add unguided | soal no.3 modul fungsi

---
+++ praktikum_4/unguided/soal_3-fungsi.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func jarak(a, b, c, d int) float64 {
+	return math.Sqrt(float64((a-c)*(a-c) + (b-d)*(b-d)))
+}
+
+func didalam(cx, cy, r, x, y int) bool {
+	return jarak(cx, cy, x, y) <= float64(r)
+}
+
+func soal_3_fungsi() {
+	var cx1, cy1, r1 int
+	fmt.Scan(&cx1, &cy1, &r1)
+
+	var cx2, cy2, r2 int
+	fmt.Scan(&cx2, &cy2, &r2)
+
+	var x, y int
+	fmt.Scan(&x, &y)
+
+	diLingkaran1 := didalam(cx1, cy1, r1, x, y)
+	diLingkaran2 := didalam(cx2, cy2, r2, x, y)
+
+	if diLingkaran1 && diLingkaran2 {
+		fmt.Println("Titik di dalam lingkaran 1 dan 2")
+	} else if diLingkaran1 {
+		fmt.Println("Titik di dalam lingkaran 1")
+	} else if diLingkaran2 {
+		fmt.Println("Titik di dalam lingkaran 2")
+	} else {
+		fmt.Println("Titik di luar lingkaran 1 dan 2")
+	}
+}
